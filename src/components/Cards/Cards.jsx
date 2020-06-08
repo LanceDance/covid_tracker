5e3a90d81bc27c2ca5b8d745c71848486313c972
petrochvat <petrochvat@gmail.com>
2020-06-08 21:46:20 +0200

    Added new feeature for counting data and added dates as well

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,38 +1,55 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid} from '@material-ui/core'
 import styles from './Cards.module.css'
-const Cards = (props) => {
-    console.log(props)
+import Countup from 'react-countup'
+import cx from 'classnames'
+const Cards = ({data :{confirmed, deaths, recovered, lastUpdate}}) => {
+    if (!confirmed) {
+        return "Loading ..."
+    }
+    console.log(confirmed)
     return (
         <div className={styles.container}>
-            <Grid container spacing={3} justify="center" >
-                <Grid item component={Card}>
+            <Grid  item component={Card} container spacing={3} justify="center" xs={12} md={3} className={cx(styles.card)} >
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
-                        <Typography text="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <Countup start= {0} 
+                            end={confirmed.value} 
+                            duration={2.5} 
+                            separator=",">
+                            </Countup>
+                        </Typography>
+                        <Typography text="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of Covid</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} container spacing={3} justify="center" xs={12} md={3} className={cx(styles.card, styles.recovered)} >
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
-                        <Typography text="textSecondary">REAL DATE</Typography>
-                        <Typography variant="body2">Number of active cases of Covid</Typography>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Countup start= {0} 
+                            end={recovered.value} 
+                            duration={2.5} 
+                            separator=",">
+                            </Countup>
+                        <Typography text="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of active recoveries of Covid</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} container spacing={3} justify="center" xs={12} md={3} className={cx(styles.card, styles.deaths)} >
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
-                        <Typography text="textSecondary">REAL DATE</Typography>
-                        <Typography variant="body2">Number of active cases of Covid</Typography>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Countup start= {0} 
+                            end={deaths.value} 
+                            duration={2.5} 
+                            separator=",">
+                            </Countup>
+                        <Typography text="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of deaths by Covid</Typography>
                     </CardContent>
                 </Grid>
 
 
-            </Grid>
 
         </div>
 
