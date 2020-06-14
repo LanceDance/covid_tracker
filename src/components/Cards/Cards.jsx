351bd3036413ca992ed35e8b3c8956f6532b000b
petrochvat <petrochvat@gmail.com>
2020-06-14 21:25:10 +0200

    add css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -10,16 +10,14 @@ const Cards = ({data :{confirmed, deaths, recovered, lastUpdate}}) => {
     console.log(confirmed)
     return (
         <div className={styles.container}>
-            <Grid  item component={Card} container spacing={3} justify="center" xs={12} md={3} className={cx(styles.card)} >
+            <Grid  item component={Card} container spacing={3} justify="center" xs={12} md={3} className={cx(styles.card, styles.infected)} >
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">
                             <Countup start= {0} 
                             end={confirmed.value} 
                             duration={2.5} 
                             separator=",">
                             </Countup>
-                        </Typography>
                         <Typography text="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of Covid</Typography>
                     </CardContent>
